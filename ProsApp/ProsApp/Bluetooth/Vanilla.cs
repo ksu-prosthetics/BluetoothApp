176c4548c24cfc1289daca763a7d6d69c3d85b6f
Tyler Rapavi <tdrapavi@gmail.com>
2022-03-01 19:43:11 -0600

    first commit

---
+++ ProsApp/ProsApp/Bluetooth/Vanilla.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+namespace ProsApp.Bluetooth
+{
+	
+		public Vanilla()
+		{
+			var ble = CrossBluetoothLE.Current;
+			var adapter = CrossBluetoothLE.Current.Adapter;
+		    
+	}
+	
+
+}
+
---
+++ ProsApp/ProsApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ProsApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ProsApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
