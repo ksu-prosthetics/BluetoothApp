f0f727c11a6eee211a7fe3685e0ce40bf81a2cad
rchevuru <rchevuru@ksu.edu>
2022-03-08 19:30:04 -0600

    in adapter.....probabaly just one big method.....wtf am i doing...

--- ProsApp/ProsApp/Bluetooth/Vanilla.cs
+++ ProsApp/ProsApp/Bluetooth/Vanilla.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plugin.BLE;
+using System;
+using System.Diagnostics;
 
 /// <summary>
 /// Summary description for Class1
@@ -6,14 +8,35 @@
 /// 
 namespace ProsApp.Bluetooth
 {
-	
-		public Vanilla()
+
+	public class Vanilla
+	{
+		public Plugin.BLE.Abstractions.Contracts.IBluetoothLE BLE;
+		Plugin.BLE.Abstractions.Contracts.IAdapter Adapter; 
+		public void Setup() //can become contructor 
 		{
-			var ble = CrossBluetoothLE.Current;
+			var ble =     CrossBluetoothLE.Current;
 			var adapter = CrossBluetoothLE.Current.Adapter;
-		    
+
+			BLE = ble; 
+			Adapter = adapter;
+		}
+		
+		public void Bluetooth()
+        {
+			var state = BLE.State;
+
+			BLE.StateChanged += (s, e) =>
+			{
+				Debug.WriteLine($"The bluetooth state changed to {e.NewState}"); 
+			}; 
+        }
+
+		/*public void ScanAdapt()
+        {
+			Adapter.DeviceDiscovered += (s, a) => deviceList.Add(a.Device);
+			await Adapter.StartScanningForDevicesAsync();
+		}*/
+
 	}
-	
-
 }
-
